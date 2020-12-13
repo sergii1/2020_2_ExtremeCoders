7e54bb654762bce691f12d954d621baeac41edbd
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-14 02:55:46 +0300

    работа над деплоем

---
+++ cmd/runAllServices.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#chmod ugo+x runFileServiceTest.sh
+cd ..
+pwd
+go run FileService/cmd/main.go &>FileService.txt &
+go run MailService/cmd/main.go &>MailService.txt &
+go run UserService/cmd/main.go &>UserService.txt &
+go run MainApplication/cmd/main.go &>MainApplication.txt
