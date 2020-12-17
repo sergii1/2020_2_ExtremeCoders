808edd8a8632f7dc5a3d1524c88ba651f7067961
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-17 07:28:00 +0300

    fux deploy script

--- cmd/runAllServices.sh
+++ cmd/runAllServices.sh
@@ -4,7 +4,14 @@
 #pwd
 echo this is deploy in path
 pwd
+sudo kill 9 `sudo lsof -t -i:8080`
+sudo kill 9 `sudo lsof -t -i:8081`
+sudo kill 9 `sudo lsof -t -i:8082`
+sudo kill 9 `sudo lsof -t -i:8083`
+sudo kill 9 `sudo lsof -t -i:8084`
+
 go run ./FileService/cmd/main.go &>FileService.txt &
 go run ./MailService/cmd/main.go &>MailService.txt &
 go run ./UserService/cmd/main.go &>UserService.txt &
 go run ./MainApplication/cmd/main.go &>MainApplication.txt &
+
