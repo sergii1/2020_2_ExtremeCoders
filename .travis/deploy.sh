74b88d874c946a5ff7201a34aab054c3e7b4fa94
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-17 10:48:14 +0300

    deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,8 +6,8 @@ ssh ubuntu@95.163.209.195
 echo DEPLLLOYMachine
 pwd
 ls
-cd go
-rm -rf 2020_2_ExtremeCoders
-git clone https://github.com/sergii1/2020_2_ExtremeCoders
-cd 2020_2_ExtremeCoders
-go run cmd/runAllServices.sh
+#cd go
+#rm -rf 2020_2_ExtremeCoders
+#git clone https://github.com/sergii1/2020_2_ExtremeCoders
+#cd 2020_2_ExtremeCoders
+go run ./cmd/runAllServices.sh
