60bd0bd04fd945723fd42ae04058e8f347588bb5
Sergiy Chepurnoy <sergeyleto@yandex.ru>
2020-12-17 08:37:21 +0300

    tmp

--- cmd/runAllServices.sh
+++ cmd/runAllServices.sh
@@ -15,4 +15,3 @@ go run ./FileService/cmd/main.go &>FileService.txt &
 go run ./MailService/cmd/main.go &>MailService.txt &
 go run ./UserService/cmd/main.go &>UserService.txt &
 go run ./MainApplication/cmd/main.go &>MainApplication.txt &
-
